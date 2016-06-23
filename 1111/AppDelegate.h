5f87f94aaa9f26b7fd05eca8fee225206762f1da
kcmaker <kc@kcmakerdeMac-mini.local>
2016-06-23 17:58:48 +0800

    first

---
+++ 1111/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  1111
+//
+//  Created by kcmaker on 16/6/23.
+//  Copyright © 2016年 kcmaker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 1111/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  1111
+//
+//  Created by kcmaker on 16/6/23.
+//  Copyright © 2016年 kcmaker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
